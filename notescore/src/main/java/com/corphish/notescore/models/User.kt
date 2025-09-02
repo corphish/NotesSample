46ecfee99f42d79742c917ea01098d260202aec9
Avinaba Dalal <d97.avinaba@gmail.com>
2025-09-02 22:39:39 +0530

    user: Make user model parcelable
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- notescore/src/main/java/com/corphish/notescore/models/User.kt
+++ notescore/src/main/java/com/corphish/notescore/models/User.kt
@@ -1,9 +1,12 @@
 package com.corphish.notescore.models
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(
     tableName = "users",
     indices = [Index(value = ["username"], unique = true)] // 👈 Make username unique
@@ -14,4 +17,4 @@ data class User(
     val displayName: String,
     val passwordHash: String,  // Store the hashed password
     val salt: String           // Store the salt used for hashing
-)
+): Parcelable
